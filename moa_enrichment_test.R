739722c23bfb70681d57981305e8857a904ccf34
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-02-21 06:19:09 -0500

    used quantiles instead of min and max

--- moa_enrichment_test.R
+++ moa_enrichment_test.R
@@ -69,7 +69,7 @@ Pf.cmpd$data <- Pf.cmpd$data %>% dplyr::filter(Metadata_pert_iname %in% (strong.
 distinct.moas <- lapply(Pf.cmpd$data$Metadata_moa, function(x) (str_split(x, "\\|")[[1]])) %>% unlist %>% 
   unique %>% setdiff(., NA)
 
-agg.fn <- function(x) return(ifelse(max(x) > -min(x), max(x), min(x)))
+agg.fn <- function(x) return(ifelse(quantile(x, 0.7) > -quantile(x, 0.3), quantile(x, 0.7), quantile(x, 0.30)))
 #agg.fn <- mean
 
 cons <- data.frame(MOA = c(), consistent = c(), sig.strn = c(), thresh = c(), n.members = c())
