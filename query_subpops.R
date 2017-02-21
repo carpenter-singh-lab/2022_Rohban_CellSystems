a1f926d4fffda01d9861b9c146ad829392f26f42
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-02-20 20:15:54 -0500

    querying subpop profiles

---
+++ query_subpops.R
@@ -0,0 +1,19 @@
+a <- read.csv("../input/profiles.csv")
+query.subpops <- function(enr.subpops, denr.subpops) {
+  cmpd <- a$compound %>% unique
+  for (x in enr.subpops) {
+    y <- a %>% dplyr::filter(str_detect(subpop.enr, sprintf("_%s_", x))) %>%
+      dplyr::select(compound) %>% as.matrix() %>% as.vector()
+    cmpd <- intersect(cmpd, y)
+  }
+  for (x in denr.subpops) {
+    y <- a %>% dplyr::filter(str_detect(subpop.denr, sprintf("_%s_", x))) %>%
+      dplyr::select(compound) %>% as.matrix() %>% as.vector()
+    cmpd <- intersect(cmpd, y)
+  }
+  return(cmpd)
+}
+
+query.subpops(enr.subpops = c("slightly elongated", "large and symmetric org. high DNA content"), denr.subpops = 
+                c("asymmetric org. high DNA content", 
+                  "small and asymmetric org. high DNA content"))
