323e70809eab97d3f6da442cbc7729062d2f390f
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-02-22 10:37:36 -0500

    + printing out all subpops of compounds related to an MOA

--- query_subpops.R
+++ query_subpops.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(stringr)
+
 a <- read.csv("../input/profiles.csv")
 query.subpops <- function(enr.subpops, denr.subpops) {
   cmpd <- a$compound %>% unique
@@ -17,3 +20,7 @@ query.subpops <- function(enr.subpops, denr.subpops) {
 query.subpops(enr.subpops = c("slightly elongated", "large and symmetric org. high DNA content"), denr.subpops = 
                 c("asymmetric org. high DNA content", 
                   "small and asymmetric org. high DNA content"))
+
+x <- readRDS("../input/repurp/2016_04_01_a549_48hr_batch1_normalized.rds")
+x <- x %>% dplyr::filter(Metadata_moa == "glucocorticoid receptor agonist") %>% dplyr::select(Metadata_pert_iname) %>% unique
+a %>% dplyr::filter(compound %in% (x %>% as.matrix() %>% as.vector())) %>% View 
