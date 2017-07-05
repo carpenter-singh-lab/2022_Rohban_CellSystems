c836754ce0eabfc4f605b0fad6078dffd206ea2d
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-07-05 08:49:16 -0400

    systematic opt.

--- rep.corr.func.R
+++ rep.corr.func.R
@@ -2,13 +2,27 @@ library(dplyr)
 library(reshape2)
 library(permute)
 
-rep.cor <- function(Pf, grp.var, feat.var) {
+rep.cor <- function(Pf, grp.var, feat.var, aux.var = NULL) {
   x <- Pf$data %>% dplyr::select(one_of(c(grp.var, feat.var))) %>%
     dplyr::group_by_(grp.var) %>% do(data.frame(cr = median(as.dist(cor(t(.[,feat.var]))), na.rm = T)))
-  return(x)
+  y <- Pf$data %>% dplyr::select(one_of(c(grp.var, feat.var))) %>%
+    #dplyr::group_by_(grp.var) %>% do(data.frame(strn = sum(abs(.[,feat.var]) > 1)/(NROW(.[,feat.var]) * 
+    #                                                                                     length(feat.var))))
+    dplyr::group_by_(grp.var) %>% do(data.frame(strn = 1))
+    
+  z <- left_join(x, y, by = grp.var)
+  if (!is.null(aux.var)) {
+    w <- Pf$data[,c(grp.var, aux.var)] %>% 
+      unique() %>%
+      left_join(., z, by = grp.var)
+  } else {
+    w <- z
+  }
+  w %<>% dplyr::mutate(mac = (cr * strn)^0.5)
+  return(w)
 }
 
-non.rep.cor.rob <- function(Pf, grp.var, feat.var) {
+non.rep.cor.rob <- function(Pf, grp.var, feat.var, quant = 0.95) {
   us <- c()
   for (i in 1:10) {
     pr <- permute::shuffle(NROW(Pf$data))
@@ -16,7 +30,7 @@ non.rep.cor.rob <- function(Pf, grp.var, feat.var) {
     u <- rep.cor(Pf, grp.var, feat.var)
     us <- rbind(us, u)
   }
-  return(quantile(us$cr, 0.95, na.rm = T))
+  return(quantile(us$cr, quant, na.rm = T))
 }
 
 non.rep.cor <- function(Pf, grp.var, feat.var, quant = 0.95) {
