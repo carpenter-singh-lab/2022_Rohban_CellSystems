238d3a6eb1fc6cb44310d19c4d5a91c1042de144
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-07-10 18:37:45 -0400

    reading all dataset in the same format

---
+++ read_dataset.R
@@ -0,0 +1,219 @@
+library(dplyr)
+library(magrittr)
+library(stringr)
+library(foreach)
+
+read.dataset <- function(data.set.name, just.bioactives = T, dose.closest = 10) {
+  if (data.set.name == "BBBC022") {
+    load("../input/Gustafsdottir/Initial_analysis.RData")
+    Pf.full.plate.norm$data %<>% filter(Image_Metadata_BROAD_ID != "" | 
+                                          (!str_detect(Well, "01") &
+                                             !str_detect(Well, "24") &
+                                             !str_detect(Well, "A") &
+                                             !str_detect(Well, "P")))
+    metad <- readr::read_csv("../input/Gustafsdottir/BBBC022_v1_image.csv")
+    metad <- metad[,c("Image_Metadata_CPD_PLATE_MAP_NAME", "Image_Metadata_PlateID")] %>% unique
+    Pf.full.plate.norm$data %<>% left_join(., metad, by = c("Plate" = "Image_Metadata_PlateID"))
+    Pf.full.plate.norm$factor_cols <- c(Pf.full.plate.norm$factor_cols, 
+                                        "Image_Metadata_CPD_PLATE_MAP_NAME")
+    
+    Pf.full.plate.norm$data %<>% dplyr::mutate(Metadata_plate_well = paste(Image_Metadata_CPD_PLATE_MAP_NAME, Well, sep = "_"))
+    Pf.full.plate.norm$factor_cols <- c(Pf.full.plate.norm$factor_cols, 
+                                        "Metadata_plate_well")
+    
+    
+    MOAs <- read.csv("../input/Gustafsdottir/MOAs.csv")
+    Pf.full.plate.norm$data %<>% 
+      dplyr::left_join(., 
+                       MOAs, 
+                       by = c("Image_Metadata_SOURCE_COMPOUND_NAME" = "Name"))
+    
+    Pf.full.plate.norm$factor_cols <- c(Pf.full.plate.norm$factor_cols, 
+                                        colnames(MOAs)) %>% unique()
+    
+    Pf.gust <- Pf.full.plate.norm
+    Pf.gust$data %<>% 
+      dplyr::rename(Metadata_broad_sample = Image_Metadata_BROAD_ID,
+                    Metadata_moa = MOA,
+                    Metadata_pert_iname = Image_Metadata_SOURCE_COMPOUND_NAME,
+                    Metadata_target = Target,
+                    Metadata_plate_map_name = Image_Metadata_CPD_PLATE_MAP_NAME,
+                    Metadata_Plate = Plate,
+                    Metadata_Well = Well)
+    
+    Pf.gust$data %<>% 
+      dplyr::mutate(Metadata_pert_iname = ifelse(Metadata_pert_iname == "", 
+                                                 "DMSO",
+                                                 as.character(Metadata_pert_iname))) %>%
+      dplyr::mutate(Metadata_pert_iname = str_to_lower(Metadata_pert_iname),
+                    Metadata_Well = str_to_lower(Metadata_Well),
+                    Metadata_Plate = str_to_lower(Metadata_Plate))
+    
+    Pf.gust$factor_cols <- c("Metadata_broad_sample",
+                             "Metadata_moa",
+                             "Metadata_pert_iname",
+                             "Metadata_target",
+                             "Metadata_plate_map_name",
+                             "Metadata_Plate",
+                             "Metadata_Well",
+                             "Metadata_plate_well")
+    
+    return(Pf.gust)
+      
+  } else if (data.set.name == "CDRP") {
+    if (just.bioactives) {
+      Pf <- readRDS("../results/master/2017-06-26_5b0bbf7c/Pf_bio_new.rds")
+    } else {
+      Pf.1 <- readRDS("../results/master/2017-06-26_5b0bbf7c/Pf_bio_new.rds")
+      Pf.2 <- readRDS("../results/master/2017-06-26_5b0bbf7c/Pf_DOS_new.rds")
+      Pf <- rbind(Pf.1, Pf.2)
+    }    
+    
+    Pf %<>% dplyr::filter(Metadata_broad_sample != "DMSO" | 
+                     (!str_detect(Metadata_Well, "01") &
+                        !str_detect(Metadata_Well, "24") &
+                        !str_detect(Metadata_Well, "a") &
+                        !str_detect(Metadata_Well, "p")))
+    
+    Pf %<>% dplyr::mutate(Metadata_plate_well = paste(Metadata_Plate_Map_Name, Metadata_Well, sep = "_"))
+    
+    feat <- Pf %>% 
+      dplyr::select(-dplyr::contains("Metadata_")) %>% 
+      colnames()
+    
+    metadata.ext <- readr::read_csv("../input/CDP2/cdrp.cpd.meta.csv")
+    brd.full <- unique(Pf$Metadata_broad_sample)
+    brds <- lapply(brd.full, function(x) paste(str_split(x,
+                                                         "-")[[1]][1:2], 
+                                               sep = "-", collapse = "-")) %>% unlist()
+    brd.mapping <- data.frame(BROAD_CPD_ID = brds, 
+                              Image_Metadata_BROAD_ID = brd.full)
+    metadata.ext %<>% 
+      dplyr::select(BROAD_CPD_ID, CPD_NAME) %>% 
+      dplyr::filter(BROAD_CPD_ID %in% brds) %>%
+      dplyr::mutate(CPD_NAME = ifelse(str_detect(CPD_NAME, "BRD-"), "", CPD_NAME)) %>%
+      dplyr::inner_join(., brd.mapping, by = "BROAD_CPD_ID") %>%
+      dplyr::select(-BROAD_CPD_ID)
+    
+    metadata <- data.frame(Metadata_broad_sample = unique(Pf$Metadata_broad_sample))
+    metadata %<>% 
+      dplyr::left_join(., metadata.ext, by = c("Metadata_broad_sample" = "Image_Metadata_BROAD_ID"))
+    
+    Pf %<>% dplyr::left_join(., metadata, by = "Metadata_broad_sample")
+    
+    Pf %<>% dplyr::mutate(CPD_NAME = str_to_lower(CPD_NAME))
+    Pf %<>% dplyr::mutate(CPD_NAME_san = str_replace_all(CPD_NAME, "-", "")) %>%
+      dplyr::mutate(CPD_NAME_san = str_replace_all(CPD_NAME_san, " ", "")) %>%
+      dplyr::mutate(CPD_NAME_san = str_replace_all(CPD_NAME_san, "\\(", "")) %>%
+      dplyr::mutate(CPD_NAME_san = str_replace_all(CPD_NAME_san, "\\)", "")) 
+    
+    MOA <- read.csv("../input/moas.txt", sep = "\t")
+    MOA %<>% dplyr::mutate(Name = str_to_lower(Name)) %>%
+      dplyr::mutate(Name_san = str_replace_all(Name, "-", "")) %>%
+      dplyr::mutate(Name_san = str_replace_all(Name_san, " ", "")) %>%
+      dplyr::mutate(Name_san = str_replace_all(Name_san, "\\(", "")) %>%
+      dplyr::mutate(Name_san = str_replace_all(Name_san, "\\)", "")) 
+    
+    Pf %<>% dplyr::left_join(., MOA, by = c("CPD_NAME_san" = "Name_san"))
+    Pf %<>% 
+      dplyr::rename(Metadata_moa = MOA,
+                    Metadata_pert_iname = CPD_NAME,
+                    Metadata_target = Target,
+                    Metadata_plate_map_name = Metadata_Plate_Map_Name)
+    
+    Pf %<>% 
+      dplyr::mutate(Metadata_pert_iname = ifelse(Metadata_broad_sample == "DMSO", 
+                                                 "DMSO",
+                                                 as.character(Metadata_pert_iname))) %>%
+      dplyr::mutate(Metadata_pert_iname = ifelse(is.na(Metadata_pert_iname) | Metadata_pert_iname == "",
+                                                 Metadata_broad_sample, 
+                                                 Metadata_pert_iname),
+                    Metadata_pert_iname = str_to_lower(Metadata_pert_iname),
+                    Metadata_Well = str_to_lower(Metadata_Well),
+                    Metadata_Plate = str_to_lower(Metadata_Plate)
+                    )
+    
+    meta.col <- setdiff(colnames(Pf), feat)
+    Pf.cdrp <- list(data = Pf, feat_cols = feat, factor_cols = meta.col)
+    
+    return(Pf.cdrp)
+  } else if (data.set.name == "Repurposing") {
+    x <- readRDS("../input/repurp/2016_04_01_a549_48hr_batch1_normalized.rds")
+    
+    x <- cbind(x, data.frame(Metadata_Treatment = paste(x$Metadata_pert_id, x$Metadata_mg_per_ml, sep = "@")))
+    feats <- colnames(x)
+    feats <- feats[which(!str_detect(feats, "Metadata"))]
+    metadata <- colnames(x)
+    metadata <- metadata[which(str_detect(metadata, "Metadata"))]
+    
+    Pf.repurp <- list(data = x, 
+                      feat_cols = feats, 
+                      factor_cols = metadata)
+    
+    Pf.repurp$data %<>% dplyr::mutate(Metadata_plate_well = paste(Metadata_plate_map_name, Metadata_Well, sep = "_"))
+    
+    Pf.repurp$factor_cols <- c(Pf.repurp$factor_cols, "Metadata_plate_well")
+    trts <- Pf.repurp$data %>% 
+      dplyr::mutate(tmp_dose = abs(Metadata_mmoles_per_liter - dose.closest)) %>%
+      dplyr::arrange(tmp_dose) %>%
+      dplyr::group_by(Metadata_pert_iname) %>%
+      dplyr::slice(1) %>%
+      dplyr::ungroup(.) %>%
+      dplyr::select(Metadata_Treatment) %>%
+      as.matrix() %>%
+      as.vector()
+    
+    Pf.repurp$data %<>%
+      dplyr::filter(Metadata_Treatment %in% c(trts, "NA@0")) %>%
+      dplyr::mutate(Metadata_pert_iname = ifelse(is.na(Metadata_pert_iname),
+                                                 "DMSO",
+                                                 Metadata_pert_iname),
+                    Metadata_pert_iname = str_to_lower(Metadata_pert_iname),
+                    Metadata_Well = str_to_lower(Metadata_Well),
+                    Metadata_Plate = str_to_lower(Metadata_Plate))
+    
+    Pf.repurp$data %<>% filter(Metadata_pert_iname != "dmso" | 
+                     (!str_detect(Metadata_Well, "01") &
+                        !str_detect(Metadata_Well, "24") &
+                        !str_detect(Metadata_Well, "a") &
+                        !str_detect(Metadata_Well, "p")))
+    
+    return(Pf.repurp)
+  } else if (data.set.name == "MOA") {
+    lst <- list.dirs("../input/MOA")
+    lst <- lst[2:length(lst)]
+    data <- foreach (l = lst, .combine = rbind) %do% {
+      pl.name <- str_split(l, "/")[[1]][4]
+      read.csv(sprintf("%s/%s_normalized.csv", l, pl.name))
+    }
+    
+    w <- apply(data, 2, function(x) any(is.na(x))) %>% which
+    w.nm <- names(w)
+    w.nm <- w.nm[which(!str_detect(w.nm, "Metadata"))]
+    w.n <- setdiff(colnames(data), w.nm)
+    data <- data[, w.n]
+    
+    data %<>% 
+      dplyr::mutate(Metadata_pert_iname = str_to_lower(Metadata_broad_sample)) %>%
+      dplyr::mutate(Metadata_broad_sample = paste(Metadata_broad_sample, 
+                                                  Metadata_cell_id, 
+                                                  Metadata_mmoles_per_liter, 
+                                                  sep = "@")) %>%
+      dplyr::mutate(Metadata_plate_well = paste(Metadata_Plate_Map_Name, Metadata_Well, sep = "_"))  %>%
+      dplyr::mutate(Metadata_Well = str_to_lower(Metadata_Well))
+    
+    y <- colnames(data)
+    y <- y[which(str_detect(y, "Metadata"))]
+    x <- setdiff(colnames(data), y)
+    
+    Pf.moa <- list(data = data, feat_cols = x, factor_cols = y)
+    Pf.moa$data %<>% filter(Metadata_pert_iname != "dmso" | 
+                                 (!str_detect(Metadata_Well, "01") &
+                                    !str_detect(Metadata_Well, "24") &
+                                    !str_detect(Metadata_Well, "a") &
+                                    !str_detect(Metadata_Well, "p")))
+    return(Pf.moa)
+  } else {
+    return(NULL)
+  }
+}
--- rep.corr.func.R
+++ rep.corr.func.R
@@ -2,24 +2,10 @@ library(dplyr)
 library(reshape2)
 library(permute)
 
-rep.cor <- function(Pf, grp.var, feat.var, aux.var = NULL) {
+rep.cor <- function(Pf, grp.var, feat.var) {
   x <- Pf$data %>% dplyr::select(one_of(c(grp.var, feat.var))) %>%
     dplyr::group_by_(grp.var) %>% do(data.frame(cr = median(as.dist(cor(t(.[,feat.var]))), na.rm = T)))
-  y <- Pf$data %>% dplyr::select(one_of(c(grp.var, feat.var))) %>%
-    #dplyr::group_by_(grp.var) %>% do(data.frame(strn = sum(abs(.[,feat.var]) > 1)/(NROW(.[,feat.var]) * 
-    #                                                                                     length(feat.var))))
-    dplyr::group_by_(grp.var) %>% do(data.frame(strn = 1))
-    
-  z <- left_join(x, y, by = grp.var)
-  if (!is.null(aux.var)) {
-    w <- Pf$data[,c(grp.var, aux.var)] %>% 
-      unique() %>%
-      left_join(., z, by = grp.var)
-  } else {
-    w <- z
-  }
-  w %<>% dplyr::mutate(mac = (cr * strn)^0.5)
-  return(w)
+  return(x)
 }
 
 non.rep.cor.rob <- function(Pf, grp.var, feat.var, quant = 0.95) {
