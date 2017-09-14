b1b7c7b76f2056d3c7ce6ce735d132347eda836c
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-09-14 12:46:52 -0400

    fixed a typo

--- rep_cor_test_cdrp.R
+++ rep_cor_test_cdrp.R
@@ -3,13 +3,14 @@ library(stringr)
 
 source("rep.corr.func.R")
 
-Pf.1 <- readRDS("../results/master/2017-09-05_e2847e98/Pf_bio_new_all.rds")
-Pf.2 <- readRDS("../results/master/2017-09-05_e2847e98/Pf_DOS_new_all.rds")
+Pf <- readRDS("../results/master/2017-09-05_e2847e98/Pf_DOS_new_all.rds")
 
-Pf <- rbind(Pf.1, Pf.2)
+Pf <- Pf %>% 
+  filter(Metadata_broad_sample != "DMSO")
+
 ft <- colnames(Pf)
 ft <- ft[which(!str_detect(ft, "Metadata_"))]
 u <- rep.cor(list(data = Pf, feat_cols = ft), grp.var = "Metadata_broad_sample", feat.var = ft)
 thr <- non.rep.cor(list(data = Pf, feat_cols = ft), grp.var = "Metadata_broad_sample", feat.var = ft, quant = 0.95)
 
-print(length(which(u$cor > thr))/NROW(u))
+print(length(which(u$cr > thr))/NROW(u))
