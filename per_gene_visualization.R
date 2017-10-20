3b70b9cd2bec62f829bf5338adae254270901a75
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-10-20 11:35:12 -0400

    added target freq.

--- per_gene_visualization.R
+++ per_gene_visualization.R
@@ -6,6 +6,19 @@ library(stringr)
 load("../results/master/2017-10-19_83a6e59e/gene_query_vignettes.RData")
 
 sign.chr <- Vectorize(function(x) ifelse(x > 0, "+", "-"))
+hist.chr <- Vectorize(function(x) {
+                        y <- unlist(str_split(x, ", "))
+                        lvls <- unique(y)
+                        hist.vec <- rep(0, length(lvls))
+                        names(hist.vec) <- lvls
+                        
+                        for (i in 1:length(y)) {
+                          hist.vec[y[i]] <- hist.vec[y[i]] + 1
+                        }
+                        
+                        hist.vec <- sort(hist.vec, decreasing = T)
+                        paste0(names(hist.vec), " (", unname(hist.vec), ")", collapse = ", ")
+                      })
 
 cmpd.annot <- gene.compound.cr.pr %>% 
    group_by(Var2) %>% 
@@ -18,7 +31,7 @@ cmpd.annot <- gene.compound.cr.pr %>%
    group_by(Var2) %>%
    summarise(Name = paste0(unique(Var1), collapse = ", "), 
              MOA = paste0(unique(MOA), collapse = ", "), 
-             Target = paste0(unique(unlist(str_split(Target, ", "))), collapse = ", ")) 
+             Target = hist.chr(paste0(Target, collapse = ", "))) 
    
 
 data.frame(gene = .gene, p.value = p.vals) %>%
@@ -29,12 +42,5 @@ data.frame(gene = .gene, p.value = p.vals) %>%
   left_join(., pthw, by = c("gene" = "Gene")) %>%
   rename(adjusted.p.value = p.value) %>%
   left_join(., cmpd.annot, by = c("gene" = "Var2")) %>%
-  rename(Gene = gene, Gene.Pathway = Pathway)
+  rename(Gene = gene, Gene.Pathway = Pathway, Compound.Name = Name) %>%
   htmlTable::htmlTable()
-
-# gene.compound.cr %>% 
-#   filter(Var2 == "MAPK14") %>% 
-#   arrange(-abs(value)) %>%
-#   slice(1:k) %>%
-#   filter(valid >= 1) %>% 
-#   View
