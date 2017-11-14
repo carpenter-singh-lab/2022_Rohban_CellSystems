7a536be71c2a0d0d029b260efb08ae3507bd13d2
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-11-14 18:05:11 -0500

    first commit

---
+++ get_toxicity_compounds.R
@@ -0,0 +1,18 @@
+## prepare the toxicity index
+
+library(dplyr)
+library(stringr)
+library(foreach)
+
+lst <- list.dirs("../input/toxicity")
+toxicity <- foreach (dr = lst[2:length(lst)]) %do% {
+  lst.sub <- list.files(dr)
+  
+  readr::read_csv(paste0(dr, "/", lst.sub[1])) %>% 
+    select(-sum.y, -sum) %>%
+    rename(Metadata_Plate = Image_Metadata_Plate, Metadata_Well = Image_Metadata_Well)
+}
+
+toxicity <- do.call(rbind, toxicity)
+
+readr::write_csv(toxicity, "toxicity.csv")
