ce4557626a531c3f48ef3c9966061206574168e6
Shantanu Singh <shsingh@broadinstitute.org>
2019-02-07 11:37:16 -0500

    Update profiling_workflow_log.sh

--- profiling_workflow_log.sh
+++ profiling_workflow_log.sh
@@ -1,1 +1,8 @@
+# ALERT - this is a stub!
+PIPELINE_SET=cellpainting_a549_20x_with_bf_with_qc_phenix_bin1
+TMPDIR=/tmp
+PROJECT_NAME=2018_02_07_Image_based_Profiling_Pilot
+BATCH_ID=2018_11_20_Batch1
+BUCKET=imaging-platform
+MAXPROCS=3
 
