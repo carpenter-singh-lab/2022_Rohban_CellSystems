9827cc358d4f17df42b63bc0850c6518b50679a3
Shantanu Singh <shsingh@broadinstitute.org>
2021-07-09 10:46:43 -0400

    p38 init

---
+++ p38a/0.knit-notebooks.R
@@ -0,0 +1,24 @@
+output_format <- "github_document"
+
+render_notebook <-
+  function(notebook_name, output_suffix = "", ...) {
+    output_file <- paste0(notebook_name, output_suffix, ".md")
+    
+    rmarkdown::render(
+      glue::glue("{notebook_name}.Rmd"),
+      output_file = output_file,
+      output_dir = "knit_notebooks",
+      output_format = output_format,
+      ...
+    )
+    
+    output_file_rel <- file.path("knit_notebooks", output_file)
+    
+    read_lines(output_file_rel) %>%
+      str_remove_all(file.path(getwd(), "knit_notebooks/")) %>%
+      write_lines(output_file_rel)
+    
+  }
+
+
+render_notebook("1.inspect-p38a-screen")
